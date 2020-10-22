a78f92cd118614f3b615e8355023d855aa2f0ca3
AJ <alexanderjessediaz@gmail.com>
2020-10-22 14:15:40 -0600

    Initial Commit

---
+++ playground-first-swift/playground_first_swiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  playground_first_swiftApp.swift
+//  playground-first-swift
+//
+//  Created by AJ Diaz on 10/22/20.
+//
+
+import SwiftUI
+
+@main
+struct playground_first_swiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ playground-first-swiftTests/playground_first_swiftTests.swift
@@ -0,0 +1,33 @@
+//
+//  playground_first_swiftTests.swift
+//  playground-first-swiftTests
+//
+//  Created by AJ Diaz on 10/22/20.
+//
+
+import XCTest
+@testable import playground_first_swift
+
+class playground_first_swiftTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
